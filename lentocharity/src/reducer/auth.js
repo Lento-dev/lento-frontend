ac8801efb11003a886287c1d4f21c3c2add09c57
zahramomeni <zahramomeninezhad@gmail.com>
2022-03-26 00:28:30 +0430

    a

---
+++ lentocharity/src/reducer/auth.js
@@ -0,0 +1,52 @@
+// import {
+//     REGISTER_SUCCESS,
+//     REGISTER_FAIL,
+//     LOGIN_SUCCESS,
+//     LOGIN_FAIL,
+//     LOGOUT,
+//   } from "../actions/actionTypes";
+  
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  const initialState = user
+    ? { isLoggedIn: true, user }
+    : { isLoggedIn: false, user: {} };
+  
+  export default function (state = initialState, action) {
+    const { type, payload } = action;
+    return null;
+    // switch (type) {
+    //   case REGISTER_SUCCESS:
+    //     return {
+    //       ...state,
+    //       isLoggedIn: false,
+    //       user: {},
+    //     };
+    //   case REGISTER_FAIL:
+    //     return {
+    //       ...state,
+    //       isLoggedIn: false,
+    //       user: {},
+    //     };
+    //   case LOGIN_SUCCESS:
+    //     return {
+    //       ...state,
+    //       isLoggedIn: true,
+    //       user: payload.user,
+    //     };
+    //   case LOGIN_FAIL:
+    //     return {
+    //       ...state,
+    //       isLoggedIn: false,
+    //       user: {},
+    //     };
+    //   case LOGOUT:
+    //     return {
+    //       ...state,
+    //       isLoggedIn: false,
+    //       user: {},
+    //     };
+    //   default:
+    //     return state;
+    // }
+  }
