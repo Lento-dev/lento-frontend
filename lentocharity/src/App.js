4a5c820c5873b0dfb5ea7331bba0077acfbd80bc
nikimajidifard <nikimajidifard.mfa@gmail.com>
2022-04-22 04:24:20 +0430

    service ad

--- lentocharity/src/App.js
+++ lentocharity/src/App.js
@@ -8,6 +8,7 @@ import SignIn from "./components/signin";
 import ForgotPassword from "./components/forgotpassword";
 import store from "./store";
 import UserInfo from './components/editprofile';
+import Serviceadvertisement from "./components/Serviceadvertisement";
 import ComplexGrid from './components/Gride';
 import User_Profile from "./components/User_Profile";
 // import Foodadvertisment from "./components/foodadvertisment";
@@ -41,6 +42,7 @@ function App() {
               <Route exact path="/edit-profile" component = { UserInfo }/>
               {/* <Route exact path="/cg" component = { Foodadvertisment }/> */}
               <Route exact path="/cloth" component = { Clothadvertisement }/>
+              <Route exact path="/service" component={Serviceadvertisement}/>
 
             </Switch>
           </BrowserRouter>
