47be3a1a881f1e768a565d9255a4cb096b914cf5
zahramomeni <zahramomeninezhad@gmail.com>
2022-05-10 11:06:22 +0430

    make test file

--- lentocharity/src/App.test.js
+++ lentocharity/src/App.test.js
@@ -4,6 +4,5 @@ import App from './App';
 const getAboutUsLink = require("./indexx");
 test("Returns about-us for english language", () => {
     expect(getAboutUsLink("en-US")).toBe("/about-us");
-    
 });
 
