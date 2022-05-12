cea5ebfd4b3d6644f3b110dddd52f0bef0ce150b
zahramomeni <zahramomeninezhad@gmail.com>
2022-05-12 19:00:24 +0430

    testt

--- lentocharity/src/App.test.js
+++ lentocharity/src/App.test.js
@@ -12,3 +12,21 @@ const isInteger = require("./isInteger");
 test("isInteger passes for integer value", () => {
     expect(isInteger(1)).toBe(true);
 });
+
+
+// const editprofile = require("./components/editprofile");
+
+// test("string", () => {
+//   expect(editprofile("zahra").toBe("zahra"))
+// })
+
+
+it("keeps submit disabled when only password provided", () => {
+  const { getByTestId } = render(<FormTest />);
+
+  const password = getByTestId("account-delete-password");
+  const submit = getByTestId("account-delete-submit");
+
+  fireEvent.change(password, { target: { value: "password" } });
+  expect(submit).toHaveClass("Mui-disabled");
+});
